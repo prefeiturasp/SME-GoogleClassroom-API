552852d0841a1fd628072c8f8be090ad09d327b0
Jeferson Cassiano <jeferson.cassiano@amcom.com.br>
2021-02-22 16:45:16 -0300

    Removido projeto de API

---
+++ sme.gclass.api.worker.rabbit/Controllers/UsuarioController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SME.GoogleClassroom.Aplicacao;
+using SME.GoogleClassroom.Infra;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SME.GoogleClassroom.Worker.Rabbit
+{
+    [ApiController]
+    [Route("api/v1/usuarios")]
+    public class UsuarioController : Controller
+    {
+        [HttpGet("{login}")]
+        [ProducesResponseType(typeof(UsuarioDto), 200)]
+        [ProducesResponseType(typeof(RetornoBaseDto), 500)]
+        [ProducesResponseType(typeof(RetornoBaseDto), 601)]
+        public async Task<IActionResult> ObterDados(string login, [FromServices] IObterDadosUsuarioPorLoginUseCase useCase)
+        {
+            return Ok(await useCase.Executar(login));
+        }
+    }
+}
