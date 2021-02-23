23c2fa807c1bcb4e06a0c9267ce94694a600e353
William Manoel dos Santos <william.santos@amcom.com.br>
2021-02-23 14:55:52 -0300

    CommandHandler de erro de usuário

---
+++ sme.gclass.api.dados/Repositorios/GoogleClassroom/RepositorioUsuarioErro.cs
@@ -0,0 +1,31 @@
+﻿using Dapper;
+using Npgsql;
+using SME.GoogleClassroom.Infra;
+using System;
+using System.Threading.Tasks;
+
+namespace SME.GoogleClassroom.Dados
+{
+    public class RepositorioUsuarioErro : IRepositorioUsuarioErro
+    {
+        private readonly ConnectionStrings connectionStrings;
+
+        public RepositorioUsuarioErro(ConnectionStrings connectionStrings)
+        {
+            this.connectionStrings = connectionStrings ?? throw new ArgumentNullException(nameof(connectionStrings));
+        }
+
+        public async Task<long> Salvar(long usuarioId, string email, string mensagem, int usuarioTipo, int execucaoTipo, DateTime dataInclusao)
+        {
+            var query = @" insert into public.usuarios_erro
+                                  (usuario_id, email, mensagem, usuario_tipo, execucao_tipo, data_inclusao)
+                           values (@usuarioId, @email, @mensagem, @usuarioTipo, @execucaoTipo, @dataInclusao) 
+                           RETURNING id";
+
+            using (var conn = new NpgsqlConnection(connectionStrings.ConnectionStringGoogleClassroom))
+            {
+                return await conn.ExecuteScalarAsync<long>(query, new { usuarioId, email, mensagem, usuarioTipo, execucaoTipo, dataInclusao });
+            }
+        }
+    }
+}
---
+++ sme.gclass.application/Commands/Usuario/SalvarUsuarioErro/SalvarUsuarioErroCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+
+namespace SME.GoogleClassroom.Aplicacao
+{
+    public class SalvarUsuarioErroCommand : IRequest<long>
+    {
+        public long UsuarioId { get; set; }
+        public string Email { get; set; }
+        public string Mensagem { get; set; }
+        public int UsuarioTipo { get; set; }
+        public int ExecucaoTipo { get; set; }
+        public DateTime DataInclusao { get; set; }        
+    }
+}
