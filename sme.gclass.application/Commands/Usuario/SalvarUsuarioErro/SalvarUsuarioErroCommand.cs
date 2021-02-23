435d9522e09369104e1d182efa6e84559abb2808
William Manoel dos Santos <william.santos@amcom.com.br>
2021-02-23 16:31:32 -0300

    Ajuste no handler

--- sme.gclass.application/Commands/Usuario/SalvarUsuarioErro/SalvarUsuarioErroCommand.cs
+++ sme.gclass.application/Commands/Usuario/SalvarUsuarioErro/SalvarUsuarioErroCommand.cs
@@ -5,6 +5,16 @@ namespace SME.GoogleClassroom.Aplicacao
 {
     public class SalvarUsuarioErroCommand : IRequest<long>
     {
+        public SalvarUsuarioErroCommand(long usuarioId, string email, string mensagem, int usuarioTipo, int execucaoTipo, DateTime dataInclusao)
+        {
+            UsuarioId = usuarioId;
+            Email = email;
+            Mensagem = mensagem;
+            UsuarioTipo = usuarioTipo;
+            ExecucaoTipo = execucaoTipo;
+            DataInclusao = dataInclusao;
+        }
+
         public long UsuarioId { get; set; }
         public string Email { get; set; }
         public string Mensagem { get; set; }
