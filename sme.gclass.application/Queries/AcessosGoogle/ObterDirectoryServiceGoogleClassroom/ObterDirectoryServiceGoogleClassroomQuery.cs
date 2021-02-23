7cd3dd43d53037e5c96a1b3671ac639e6d8ec93f
Jeferson Cassiano <jeferson.cassiano@amcom.com.br>
2021-02-23 16:12:31 -0300

    Ajuste handlers obter serviços google classroom

---
+++ sme.gclass.application/Queries/AcessosGoogle/ObterDirectoryServiceGoogleClassroom/ObterDirectoryServiceGoogleClassroomQuery.cs
@@ -0,0 +1,9 @@
+﻿using Google.Apis.Admin.Directory.directory_v1;
+using MediatR;
+
+namespace SME.GoogleClassroom.Aplicacao
+{
+    public class ObterDirectoryServiceGoogleClassroomQuery : IRequest<DirectoryService>
+    {
+    }
+}
