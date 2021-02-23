425ff754e25ab6ad369ef74076c687b6e50d07d5
Jeferson Cassiano <jeferson.cassiano@amcom.com.br>
2021-02-23 14:22:47 -0300

    Handler adicionar erro curso

---
+++ sme.gclass.api.dados/Repositorios/GoogleClassroom/RepositorioCursoErro.cs
@@ -0,0 +1,39 @@
+﻿using Dapper;
+using Npgsql;
+using SME.GoogleClassroom.Infra;
+using System;
+using System.Threading.Tasks;
+
+namespace SME.GoogleClassroom.Dados
+{
+    public class RepositorioCursoErro : IRepositorioCursoErro
+    {
+        private readonly ConnectionStrings ConnectionStrings;
+
+        public RepositorioCursoErro(ConnectionStrings connectionStrings)
+        {
+            this.ConnectionStrings = connectionStrings ?? throw new ArgumentNullException(nameof(connectionStrings));
+        }
+
+        public async Task<long> SalvarAsync(CursoErroDto dto)
+        {
+            var query = @" INSERT INTO cursos_erro 
+                            (turma_id, componente_curricular_id, mensagem, execucao_tipo, curso_id, data_inclusao)
+                            VALUES 
+                            (@TurmaId, @ComponenteCurricularId, @Mensagem, @ExecucaoTipo, @CursoId, @DataInclusao)";
+
+            var parametros = new
+            {
+                dto.TurmaId,
+                dto.ComponenteCurricularId,
+                dto.Mensagem,
+                ExecucaoTipo = (int)dto.ExecucaoTipo,
+                dto.CursoId,
+                dto.DataInclusao
+            };
+
+            using var conn = new NpgsqlConnection(ConnectionStrings.ConnectionStringGoogleClassroom);
+            return await conn.ExecuteAsync(query, parametros);
+        }
+    }
+}
---
+++ sme.gclass.application/Commands/Curso/InserirCursoErro/InserirCursoErroCommand.cs
@@ -0,0 +1,42 @@
+﻿using FluentValidation;
+using MediatR;
+using SME.GoogleClassroom.Infra;
+
+namespace SME.GoogleClassroom.Aplicacao
+{
+    public class InserirCursoErroCommand : IRequest<long>
+    {
+        public int TurmaId { get; set; }
+
+        public int ComponenteCurricularId { get; set; }
+
+        public string Mensagem { get; set; }
+
+        public long? CursoId { get; set; }
+
+        public ExecucaoTipo ExecucaoTipo { get; set; }
+    }
+
+    public class InserirCursoErroCommandValidator : AbstractValidator<InserirCursoErroCommand>
+    {
+        public InserirCursoErroCommandValidator()
+        {
+
+            RuleFor(c => c.TurmaId)
+                .NotEmpty()
+                .WithMessage("O Id da Turma deve ser informado.");
+
+            RuleFor(c => c.ComponenteCurricularId)
+                .NotEmpty()
+                .WithMessage("O Componente Curricular deve ser informado.");
+
+            RuleFor(c => c.Mensagem)
+                .NotEmpty()
+                .WithMessage("A mensagem deve ser informada.");
+
+            RuleFor(c => c.ExecucaoTipo)
+               .NotEmpty()
+               .WithMessage("O tipo da execução deve ser informada.");
+        }
+    }
+}
