075aaf5e7cddbd1207144559b39f5b31c0d1c52a
Jeferson Cassiano <jeferson.cassiano@amcom.com.br>
2021-02-24 15:14:24 -0300

    Handler obter data ultima execucao por tipo

---
+++ sme.gclass.application/Queries/ExecucaoControle/ObterDataUltimaExecucaoPorTipo/ObterDataUltimaExecucaoPorTipoQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using SME.GoogleClassroom.Dados;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SME.GoogleClassroom.Aplicacao
+{
+    public class ObterDataUltimaExecucaoPorTipoQueryHandler : IRequestHandler<ObterDataUltimaExecucaoPorTipoQuery, DateTime>
+    {
+        private readonly IRepositorioExecucaoControle repositorioExecucaoControle;
+
+        public ObterDataUltimaExecucaoPorTipoQueryHandler(IRepositorioExecucaoControle repositorioExecucaoControle)
+        {
+            this.repositorioExecucaoControle = repositorioExecucaoControle ?? throw new ArgumentNullException(nameof(repositorioExecucaoControle));
+        }
+        public async Task<DateTime> Handle(ObterDataUltimaExecucaoPorTipoQuery request, CancellationToken cancellationToken)
+        {
+            var dataUltimaExecucao = await repositorioExecucaoControle.ObterDataUltimaExecucaoPorTipo(request.ExecucaoTipo);
+
+            if (dataUltimaExecucao == null)
+                return DateTime.MinValue;
+
+            return dataUltimaExecucao;
+        }
+    }
+}
