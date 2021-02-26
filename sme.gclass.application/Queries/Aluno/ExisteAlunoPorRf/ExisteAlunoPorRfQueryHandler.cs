6638dd2e03fcd7721b64e06e608d0ead4d7f20ce
eliton-amcom <eliton.santos@amcom.com.br>
2021-02-26 19:10:20 -0300

    34976 - QueryHandler verificar se aluno existe pelo rf

---
+++ sme.gclass.application/Queries/Aluno/ExisteAlunoPorRf/ExisteAlunoPorRfQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using SME.GoogleClassroom.Dados;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SME.GoogleClassroom.Aplicacao
+{
+    public class ExisteAlunoPorRfQueryHandler : IRequestHandler<ExisteAlunoPorRfQuery, bool>
+    {
+        private readonly IRepositorioUsuario repositorioUsuario;
+
+        public ExisteAlunoPorRfQueryHandler(IRepositorioUsuario repositorioUsuario)
+        {
+            this.repositorioUsuario = repositorioUsuario ?? throw new ArgumentNullException(nameof(repositorioUsuario));
+        }
+        public async Task<bool> Handle(ExisteAlunoPorRfQuery request, CancellationToken cancellationToken)
+        {
+            var existe = await repositorioUsuario.ExisteAlunoPorRf(request.Rf);
+            return existe;
+        }
+    }
+
+}
