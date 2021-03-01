8279a2c78311388f53a204c31c167fce225aee17
Jeferson Cassiano <jeferson.cassiano@amcom.com.br>
2021-03-01 17:30:32 -0300

    Criação validação email existente

---
+++ sme.gclass.application/Queries/Aluno/VerificarEmailExistente/VerificarEmailExistenteAlunoQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using SME.GoogleClassroom.Dados;
+using SME.GoogleClassroom.Dominio;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SME.GoogleClassroom.Aplicacao
+{
+    public class VerificarEmailExistenteAlunoQueryHandler : IRequestHandler<VerificarEmailExistenteAlunoQuery, AlunoEol>
+    {
+        private readonly IRepositorioUsuario repositorioUsuario;
+        public VerificarEmailExistenteAlunoQueryHandler(IRepositorioUsuario repositorioUsuario)
+        {
+            this.repositorioUsuario = repositorioUsuario ?? throw new ArgumentNullException(nameof(repositorioUsuario));
+        }
+        public async Task<AlunoEol> Handle(VerificarEmailExistenteAlunoQuery request, CancellationToken cancellationToken)
+        {
+            return await ObterEmailValido(request.AlunoEol);
+        }
+
+        private async Task<AlunoEol> ObterEmailValido(AlunoEol aluno, int tentativa = 0)
+        {
+            aluno.DefinirEmail(tentativa);
+            var emailExistente = await repositorioUsuario.ObterEmailUsuarioPorTipo(aluno.Email, 1);
+
+            if (string.IsNullOrEmpty(emailExistente))
+                return aluno;
+            else
+                return await ObterEmailValido(aluno, tentativa + 1);
+
+        }
+    }
+}
