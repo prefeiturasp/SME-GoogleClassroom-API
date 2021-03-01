70d04bdcda83b71d2f13a1984ac65443a7111f94
Carlos Dias <carlos.dias@amcom.com.br>
2021-03-01 13:00:08 -0300

    CM - Ajuste de testes unitários.

--- SME.GoogleClassroom.Testes.Unitario/FuncionarioTeste.cs
+++ SME.GoogleClassroom.Testes.Unitario/FuncionarioTeste.cs
@@ -6,36 +6,37 @@ namespace SME.GoogleClassroom.Testes
     public class FuncionarioTeste
     {
         [Theory(DisplayName = "Geração de Email"), MemberData(nameof(CorrectData))]
-        public void GeracaoDeEmail(long rf, string nome, string emailEsperado)
+        public void GeracaoDeEmail(long rf, string nome, string organizationPath, string emailEsperado)
         {
-            FuncionarioEol funcionario = new FuncionarioEol(rf, nome,"");
+            FuncionarioEol funcionario = new FuncionarioEol(rf, nome, organizationPath);
 
             Assert.Equal(emailEsperado, funcionario.Email);
         }
 
         [Theory(DisplayName = "Geração de Email"), MemberData(nameof(WrongData))]
-        public void GeracaoDeEmailErrado(long rf, string nome, string emailEsperado)
+        public void GeracaoDeEmailErrado(long rf, string nome, string organizationPath, string emailEsperado)
         {
-            FuncionarioEol funcionario = new FuncionarioEol(rf, nome, "");
+            FuncionarioEol funcionario = new FuncionarioEol(rf, nome, organizationPath);
 
             Assert.NotEqual(emailEsperado, funcionario.Email);
         }
 
         public static readonly object[][] CorrectData =
         {
-            new object[] { 0, "", "" },
-            new object[] { 1234567 , "Maria Jesus", "mariajesus.1234567@edu.sme.prefeitura.sp.gov.br" },
-            new object[] { 1234567, "Maria Tereza Jesus", "mariajesus.1234567@edu.sme.prefeitura.sp.gov.br" },
-            new object[] { 1234567, "Maria Tereza Santos Jesus", "mariajesus.1234567@edu.sme.prefeitura.sp.gov.br" },
-            new object[] { 1234567, "Maria Tereza de Santos Jesus", "mariajesus.1234567@edu.sme.prefeitura.sp.gov.br" }
+            new object[] { 0, "", "", "" },
+            new object[] { 1234567 , "Maria Jesus", "/FUNCIONARIO", "mariajesus.1234567@edu.sme.prefeitura.sp.gov.br" },
+            new object[] { 1234567, "Maria Tereza Jesus", "/FUNCIONARIO", "mariatjesus.1234567@edu.sme.prefeitura.sp.gov.br" },
+            new object[] { 1234567, "Maria Tereza Santos Jesus", "/FUNCIONARIO", "mariatsjesus.1234567@edu.sme.prefeitura.sp.gov.br" },
+            new object[] { 1234567, "Maria Tereza de Santos Jesus", "/FUNCIONARIO", "mariatsjesus.1234567@edu.sme.prefeitura.sp.gov.br" },
+            new object[] { 1234567, "João da Silva", "/FUNCIONARIO", "joaosilva.1234567@edu.sme.prefeitura.sp.gov.br" }
         };
 
         public static readonly object[][] WrongData =
         {
-            new object[] { 1234567, "Maria Jesus", "mariaesus.1234567@edu.sme.prefeitura.sp.gov.br" },
-            new object[] { 1234567, "Maria Tereza Jesus", "marjesus.01022017@edu.sme.prefeitura.sp.gov.br" },
-            new object[] { 1234567, "Maria Tereza Santos Jesus", "marisus.01032017@edu.sme.prefeitura.sp.gov.br" },
-            new object[] { 1234567, "Maria Tereza de Santos Jesus", "sjesus.06061992@edu.sme.prefeitura.sp.gov.br" }
+            new object[] { 1234567, "Maria Jesus", "/FUNCIONARIO", "mariaesus.1234567@edu.sme.prefeitura.sp.gov.br" },
+            new object[] { 1234567, "Maria Tereza Jesus", "/FUNCIONARIO", "marjesus.01022017@edu.sme.prefeitura.sp.gov.br" },
+            new object[] { 1234567, "Maria Tereza Santos Jesus", "/FUNCIONARIO", "marisus.01032017@edu.sme.prefeitura.sp.gov.br" },
+            new object[] { 1234567, "Maria Tereza de Santos Jesus", "/FUNCIONARIO", "sjesus.06061992@edu.sme.prefeitura.sp.gov.br" }
         };
     }
 }
