10558ec137b828135d295c87016fc27ed858538f
Carlos Dias <carlos.dias@amcom.com.br>
2021-03-02 09:07:47 -0300

    CM - Aplicação da regra de trucamento se passar de 60 caracteres.

--- SME.GoogleClassroom.Testes.Unitario/FuncionarioTeste.cs
+++ SME.GoogleClassroom.Testes.Unitario/FuncionarioTeste.cs
@@ -5,6 +5,19 @@ namespace SME.GoogleClassroom.Testes
 {
     public class FuncionarioTeste
     {
+        [Theory(DisplayName = "Formatação de nomes")]
+        [InlineData("", "", "")]
+        [InlineData("Carlos Augusto Ferreira Dias", "Augusto Ferreira Dias", "Carlos Augusto Ferreira Dias")]
+        [InlineData("Maria Aparecida da Silva Agnaldo José Kentucky Gonçalvez de Lemos Fischer", "Aparecida da Silva Agnaldo José Kentucky Gonçalvez de Lemos", "Maria Aparecida da Silva Agnaldo José Kentucky Gonçalvez de Lemos")]
+        public void FormatacaoNomes(string nome, string sobrenomeEsperado, string nomeEsperado)
+        {
+            var professor = new ProfessorGoogle(1, nome, "email", "organizationPath");
+
+            Assert.NotNull(professor);
+            Assert.Equal(sobrenomeEsperado, professor.Sobrenome);
+            Assert.Equal(nomeEsperado, professor.Nome);
+        }
+
         [Theory(DisplayName = "Geração de Email"), MemberData(nameof(CorrectData))]
         public void GeracaoDeEmail(long rf, string nome, string organizationPath, string emailEsperado)
         {
