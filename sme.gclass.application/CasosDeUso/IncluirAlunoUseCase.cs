046e8915f3fed0cfd8d583b901c3ae1d764f0b7b
Jeferson Cassiano <jeferson.cassiano@amcom.com.br>
2021-03-02 16:37:37 -0300

    Use case aluno incluir

---
+++ sme.gclass.application/CasosDeUso/IncluirAlunoUseCase.cs
@@ -0,0 +1,43 @@
+﻿using MediatR;
+using Newtonsoft.Json;
+using SME.GoogleClassroom.Dominio;
+using SME.GoogleClassroom.Infra;
+using System;
+using System.Threading.Tasks;
+
+namespace SME.GoogleClassroom.Aplicacao
+{
+    public class IncluirAlunoUseCase : IIncluirAlunoUseCase
+    {
+        private readonly IMediator mediator;
+
+        public IncluirAlunoUseCase(IMediator mediator)
+        {
+            this.mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        }
+
+        public async Task<bool> Executar(MensagemRabbit mensagemRabbit)
+        {
+            var resposta = JsonConvert.DeserializeObject<AlunoParaInclusaoDto>(mensagemRabbit.Mensagem.ToString());
+
+            if (resposta != null)
+            {
+                var alunoExistente = await mediator.Send(new ExisteAlunoPorRfQuery(resposta.Codigo));
+
+                if (!alunoExistente)
+                {
+                    var alunoEol = MapearAlunoDtoParaDominio(resposta);
+
+                    alunoEol  = await mediator.Send(new VerificarEmailExistenteAlunoQuery(alunoEol));
+                }
+            }
+
+            return true;
+        }
+
+        private AlunoEol MapearAlunoDtoParaDominio(AlunoParaInclusaoDto resposta)
+        {
+            return new AlunoEol(resposta.Codigo, resposta.Nome, resposta.CaminhoOrganizacao, resposta.DataNascimento);
+        }
+    }
+}
