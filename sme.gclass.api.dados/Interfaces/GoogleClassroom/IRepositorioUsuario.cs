75675522703923c80460e27becc8b20412302e42
Carlos Dias <carlos.dias@amcom.com.br>
2021-03-03 17:17:35 -0300

    CM - Ajuste pós-merge

--- sme.gclass.api.dados/Interfaces/GoogleClassroom/IRepositorioUsuario.cs
+++ sme.gclass.api.dados/Interfaces/GoogleClassroom/IRepositorioUsuario.cs
@@ -2,7 +2,6 @@
 using SME.GoogleClassroom.Infra;
 using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace SME.GoogleClassroom.Dados
@@ -10,13 +9,23 @@ namespace SME.GoogleClassroom.Dados
     public interface IRepositorioUsuario
     {
         Task<PaginacaoResultadoDto<AlunoGoogle>> ObterAlunosAsync(Paginacao paginacao, long? codigoEol, string email);
+
         Task<bool> ExisteAlunoPorRf(long rf);
+
+        Task<string> ObterEmailUsuarioPorTipo(string email, int usuarioTipo);
+
         Task<PaginacaoResultadoDto<FuncionarioGoogle>> ObterFuncionariosAsync(Paginacao paginacao, long? rf, string email);
-        Task<PaginacaoResultadoDto<ProfessorGoogle>> ObterProfessoresAsync(Paginacao paginacao,long? rf, string email);
+
+        Task<PaginacaoResultadoDto<ProfessorGoogle>> ObterProfessoresAsync(Paginacao paginacao, long? rf, string email);
+
         Task<IEnumerable<FuncionarioGoogle>> ObterFuncionariosPorRfs(long[] rfs);
+
         Task<bool> ExisteFuncionarioPorRf(long rf);
+
         Task<IEnumerable<ProfessorGoogle>> ObterProfessoresPorRfs(long[] rfs);
+
         Task<bool> ExisteProfessorPorRf(long rf);
+
         Task<long> SalvarAsync(long id, string nome, string email, UsuarioTipo tipo, string organizationPath, DateTime dataInclusao, DateTime? dataAtualizacao);
     }
 }
