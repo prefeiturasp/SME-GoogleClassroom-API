124b2c79c638b64326253d2b45428ec143443c41
Carlos Dias <carlos.dias@amcom.com.br>
2021-03-04 10:35:32 -0300

    CM - Melhorias nos parâmetros dos endpoints

---
+++ sme.gclass.api.infra/Dtos/Aluno/FiltroObterAlunosCadastradosDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SME.GoogleClassroom.Infra
+{
+    public class FiltroObterAlunosCadastradosDto : FiltroPaginacaoBaseDto
+    {
+        public long? CodigoEol { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ sme.gclass.api.infra/Dtos/Aluno/FiltroObterAlunosIncluirGoogleDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace SME.GoogleClassroom.Infra
+{
+    public class FiltroObterAlunosIncluirGoogleDto : FiltroPaginacaoBaseDto
+    {
+        public long CodigoEol { get; set; }
+
+        [Required]
+        [DefaultValue("2021-02-20")]
+        public DateTime UltimaExecucao { get; set; }
+    }
+}
