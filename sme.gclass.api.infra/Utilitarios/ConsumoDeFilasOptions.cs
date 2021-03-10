dc97b2f452bf43d155e5e336009bd0bfb0b19f43
Carlos Dias <carlosafdias@outlook.com>
2021-03-10 17:29:13 -0300

    CM - Criação de separação de filas para o worker por variável de ambiente.

---
+++ sme.gclass.api.infra/Utilitarios/ConsumoDeFilasOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace SME.GoogleClassroom.Infra
+{
+    public class ConsumoDeFilasOptions
+    {
+        public const string Secao = "ConsumoDeFila";
+        public bool ConsumirFilasSync { get; set; }
+        public bool ConsumirFilasDeInclusao { get; set; }
+    }
+}
