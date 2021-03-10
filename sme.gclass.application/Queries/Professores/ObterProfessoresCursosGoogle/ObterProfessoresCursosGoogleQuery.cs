df1f82e04f94ce7c684d76982713bfacd623cf69
William Manoel dos Santos <william.santos@amcom.com.br>
2021-03-10 06:48:06 -0300

    Criar endpoint de professoresxcursos já cadastrados

---
+++ sme.gclass.application/Queries/Professores/ObterProfessoresCursosGoogle/ObterProfessoresCursosGoogleQuery.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation;
+using MediatR;
+using SME.GoogleClassroom.Dominio;
+using SME.GoogleClassroom.Infra;
+
+namespace SME.GoogleClassroom.Aplicacao
+{
+    public class ObterProfessoresCursosGoogleQuery : IRequest<PaginacaoResultadoDto<ProfessorCursosCadastradosDto>>
+    {
+        public ObterProfessoresCursosGoogleQuery(Paginacao paginacao, long? rf, long? turmaId, long? componenteCurricularId)
+        {
+            Paginacao = paginacao;
+            Rf = rf;
+            TurmaId = turmaId;
+            ComponenteCurricularId = componenteCurricularId;
+        }
+
+        public Paginacao Paginacao { get; set; }
+        public long? Rf { get; set; }
+        public long? TurmaId { get; set; }
+        public long? ComponenteCurricularId { get; set; }
+    }
+    public class ObterProfessoresCursosGoogleQueryValidator : AbstractValidator<ObterProfessoresCursosGoogleQuery>
+    {
+        public ObterProfessoresCursosGoogleQueryValidator()
+        {
+            RuleFor(x => x.Paginacao)
+                .NotEmpty()
+                .WithMessage("A paginação deve ser informada.");
+
+            RuleFor(x => x.Paginacao.QuantidadeRegistros)
+                .NotEmpty()
+                .WithMessage("O número da página e a quantidade de registro devem ser informados.");
+        }
+    }
+}
---
+++ sme.gclass.application/Queries/Professores/ObterProfessoresCursosGoogle/ObterProfessoresCursosGoogleQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using SME.GoogleClassroom.Dados;
+using SME.GoogleClassroom.Infra;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SME.GoogleClassroom.Aplicacao
+{
+    public class ObterProfessoresCursosGoogleQueryHandler : IRequestHandler<ObterProfessoresCursosGoogleQuery, PaginacaoResultadoDto<ProfessorCursosCadastradosDto>>
+    {
+        private readonly IRepositorioCursoUsuario repositorioCursoUsuario;
+
+        public ObterProfessoresCursosGoogleQueryHandler(IRepositorioCursoUsuario repositorioCursoUsuario)
+        {
+            this.repositorioCursoUsuario = repositorioCursoUsuario ?? throw new ArgumentNullException(nameof(repositorioCursoUsuario));
+        }
+
+        public async Task<PaginacaoResultadoDto<ProfessorCursosCadastradosDto>> Handle(ObterProfessoresCursosGoogleQuery request, CancellationToken cancellationToken)
+            => await repositorioCursoUsuario.ObterProfessoresCursosAsync(request.Paginacao, request.Rf, request.TurmaId, request.ComponenteCurricularId);
+    }
+}
