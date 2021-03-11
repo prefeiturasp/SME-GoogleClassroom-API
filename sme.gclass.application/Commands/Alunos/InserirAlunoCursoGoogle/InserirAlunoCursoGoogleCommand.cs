b2ce762c1f672a965f487f8fd4dcec03ccc245d9
William Manoel dos Santos <william.santos@amcom.com.br>
2021-03-11 11:09:40 -0300

    Inicio Dev

---
+++ sme.gclass.application/Commands/Alunos/InserirAlunoCursoGoogle/InserirAlunoCursoGoogleCommand.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+using MediatR;
+using SME.GoogleClassroom.Dominio;
+
+namespace SME.GoogleClassroom.Aplicacao
+{
+    public class InserirAlunoCursoGoogleCommand : IRequest<bool>
+    {
+        public InserirAlunoCursoGoogleCommand(AlunoCursoGoogle alunoCursoGoogle, string email)
+        {
+            AlunoCursoGoogle = alunoCursoGoogle;
+            Email = email;
+        }
+
+        public AlunoCursoGoogle AlunoCursoGoogle { get; set; }
+        public string Email { get; set; }
+    }
+
+    public class InserirAlunoCursoGoogleCommandValidator : AbstractValidator<InserirAlunoCursoGoogleCommand>
+    {
+        public InserirAlunoCursoGoogleCommandValidator()
+        {
+            RuleFor(x => x.AlunoCursoGoogle)
+                .NotEmpty()
+                .WithMessage("É necessário informar um aluno x curso que será incluído no Google Classroom.");
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .WithMessage("O e-mail do aluno deve ser informado.");
+        }
+    }
+}
---
+++ sme.gclass.application/Queries/Aluno/ExisteAlunoCursoGoogle/ExisteAlunoCursoGoogleQueryHandler.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SME.GoogleClassroom.Aplicacao
+{
+    public class ExisteAlunoCursoGoogleQueryHandler : IRequestHandler<ExisteAlunoCursoGoogleQuery, bool>
+    {
+        public Task<bool> Handle(ExisteAlunoCursoGoogleQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
