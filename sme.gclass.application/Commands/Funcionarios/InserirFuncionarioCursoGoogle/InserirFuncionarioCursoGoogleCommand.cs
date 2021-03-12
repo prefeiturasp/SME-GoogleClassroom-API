754e16936596aeb6124e90bb2a9d01c4340d2cbf
William Manoel dos Santos <william.santos@amcom.com.br>
2021-03-12 16:10:38 -0300

    Ajuste no command

--- sme.gclass.application/Commands/Funcionarios/InserirFuncionarioCursoGoogle/InserirFuncionarioCursoGoogleCommand.cs
+++ sme.gclass.application/Commands/Funcionarios/InserirFuncionarioCursoGoogle/InserirFuncionarioCursoGoogleCommand.cs
@@ -1,11 +1,31 @@
-﻿using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using FluentValidation;
+using MediatR;
+using SME.GoogleClassroom.Dominio;
 
 namespace SME.GoogleClassroom.Aplicacao
 {
     public class InserirFuncionarioCursoGoogleCommand : IRequest<bool>
     {
+        public InserirFuncionarioCursoGoogleCommand(FuncionarioCursoGoogle funcionarioCursoGoogle, string email)
+        {
+            FuncionarioCursoGoogle = funcionarioCursoGoogle;
+            Email = email;
+        }
+
+        public FuncionarioCursoGoogle FuncionarioCursoGoogle { get; set; }
+        public string Email { get; set; }
+    }
+    public class InserirFuncionarioCursoGoogleCommandValidator : AbstractValidator<InserirFuncionarioCursoGoogleCommand>
+    {
+        public InserirFuncionarioCursoGoogleCommandValidator()
+        {
+            RuleFor(x => x.FuncionarioCursoGoogle)
+                .NotEmpty()
+                .WithMessage("É necessário informar um funcionario x curso que será incluído no Google Classroom.");
+
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .WithMessage("O e-mail do funcionario deve ser informado.");
+        }
     }
 }
