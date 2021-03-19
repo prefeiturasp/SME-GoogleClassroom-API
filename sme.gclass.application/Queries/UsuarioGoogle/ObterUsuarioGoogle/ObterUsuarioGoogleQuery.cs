81f9667326ba6f2a1519b46e4a77d4aecf405d7c
William Manoel dos Santos <william.santos@amcom.com.br>
2021-03-19 14:34:20 -0300

    Query de consulta usuário google

---
+++ sme.gclass.application/Queries/UsuarioGoogle/ObterUsuarioGoogle/ObterUsuarioGoogleQuery.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using MediatR;
+using SME.GoogleClassroom.Dominio;
+
+namespace SME.GoogleClassroom.Aplicacao
+{
+    public class ObterUsuarioGoogleQuery : IRequest<UsuarioGoogle>
+    {
+        public ObterUsuarioGoogleQuery(string email)
+        {
+            Email = email;
+        }
+
+        public string Email { get; set; }
+    }
+
+    public class ObterUsuarioGoogleQueryValidator : AbstractValidator<ObterUsuarioGoogleQuery>
+    {
+        public ObterUsuarioGoogleQueryValidator()
+        {
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .WithMessage("o Email do usuário deve ser informado.");
+        }
+    }
+}
