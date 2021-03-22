5351eb8cb7d57fddaa0777126f2a169b041143e4
William Manoel dos Santos <william.santos@amcom.com.br>
2021-03-22 11:06:06 -0300

    Ajuste no script e inclusão de coluna email em cruso_usuário_erro

--- scripts/V21__ALTERACAO_USUARIOS.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE usuarios ADD CONSTRAINT email_unique UNIQUE (email);
-
-ALTER TABLE usuarios  ALTER column id DROP NOT NULL;    
-
-CREATE INDEX usuarios_id_idx ON public.usuarios USING btree (id);
-
-CREATE INDEX usuarios_usuario_tipo_idx ON public.usuarios USING btree (usuario_tipo);
