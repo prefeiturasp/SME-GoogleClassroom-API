542a02da005bd9f4ccb16f2382598759222d0b22
Carlos Dias <carlosafdias@outlook.com>
2021-03-25 09:21:24 -0300

    CM - Ajuste de pastas

---
+++ sme.gclass.application/CasosDeUso/Alunos/Erros/TrataSyncGoogleAlunoErrosUseCase.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using SME.GoogleClassroom.Aplicacao.Interfaces;
+using SME.GoogleClassroom.Infra;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SME.GoogleClassroom.Aplicacao
+{
+    public class TrataSyncGoogleAlunoErrosUseCase : ITrataSyncGoogleAlunoErrosUseCase
+    {
+        private readonly IMediator mediator;
+
+        public TrataSyncGoogleAlunoErrosUseCase(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        public async Task<bool> Executar(MensagemRabbit mensagemRabbit)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
