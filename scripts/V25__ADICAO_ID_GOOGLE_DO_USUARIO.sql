4a639147d0e3be55222e15bc110ddabc24609ea2
Carlos Dias <carlosafdias@outlook.com>
2021-04-26 09:00:43 -0300

    CM -Adicionar a condição if not exists

--- scripts/V25__ADICAO_ID_GOOGLE_DO_USUARIO.sql
+++ scripts/V25__ADICAO_ID_GOOGLE_DO_USUARIO.sql
@@ -1,4 +1,4 @@
-ALTER TABLE public.usuarios ADD google_classroom_id int8 NULL;
+ALTER TABLE public.usuarios ADD if not exists google_classroom_id int8 NULL;
 
-CREATE INDEX usuarios_google_classroom_id_idx ON public.usuarios USING btree (google_classroom_id);
+CREATE index if not exists usuarios_google_classroom_id_idx ON public.usuarios USING btree (google_classroom_id);
 
