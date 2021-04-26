ce98a1e4af9b5e2dd489100e0a82a51d93e0cbba
elitonds <elitonds@gmail.com>
2021-04-26 14:37:37 -0300

    ajuste

---
+++ sme.gclass.api.domain/Entidades/Google/Comparativo/UsuarioCurso/UsuarioCursoComparativo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.GoogleClassroom.Dominio
+{
+    public class UsuarioCursoComparativo
+    {
+        public string UsuarioId { get; set; }
+        public string CursoId { get; set; }
+    }
+}
