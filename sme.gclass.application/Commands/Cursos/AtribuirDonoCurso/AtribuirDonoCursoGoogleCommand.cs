75032ee5c5810695dd0728d94bffb9f5bc76c152
elitonds <elitonds@gmail.com>
2021-05-12 17:25:07 -0300

    ajuste mensagens

--- sme.gclass.application/Commands/Cursos/AtribuirDonoCurso/AtribuirDonoCursoGoogleCommand.cs
+++ sme.gclass.application/Commands/Cursos/AtribuirDonoCurso/AtribuirDonoCursoGoogleCommand.cs
@@ -1,4 +1,5 @@
-﻿using MediatR;
+﻿using FluentValidation;
+using MediatR;
 
 namespace SME.GoogleClassroom.Aplicacao
 {
@@ -11,5 +12,18 @@ namespace SME.GoogleClassroom.Aplicacao
         }
         public long CursoId { get; set; }
         public string UsuarioId { get; set; }
+    }
+
+    public class AtribuirDonoCursoGoogleCommandValidator : AbstractValidator<AtribuirDonoCursoGoogleCommand>
+    {
+        public AtribuirDonoCursoGoogleCommandValidator()
+        {
+            RuleFor(x => x.CursoId)
+                .NotEmpty()
+                .WithMessage("O curso deve ser informado para atribuir o dono.");
+            RuleFor(x => x.UsuarioId)
+                .NotEmpty()
+                .WithMessage("O usuario deve ser informado para atribuir o dono.");
+        }
     }
 }
--- sme.gclass.application/Commands/Cursos/AtribuirDonoCurso/AtribuirDonoCursoGoogleCommandHandler.cs
+++ sme.gclass.application/Commands/Cursos/AtribuirDonoCurso/AtribuirDonoCursoGoogleCommandHandler.cs
@@ -8,6 +8,7 @@ using SME.GoogleClassroom.Infra;
 using SME.GoogleClassroom.Infra.Interfaces.Metricas;
 using SME.GoogleClassroom.Infra.Politicas;
 using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -33,14 +34,19 @@ namespace SME.GoogleClassroom.Aplicacao
 
         private async Task AtribuirDonoCurso(long cursoId, string ownerId, ClassroomService servicoClassroom)
         {
-            var professor = await servicoClassroom.Courses.Teachers.Get(cursoId.ToString(), ownerId).ExecuteAsync();
+            var professores = await servicoClassroom.Courses.Teachers.List(cursoId.ToString()).ExecuteAsync();
+            var professor = professores.Teachers.FirstOrDefault(p => p.UserId == ownerId);
             if (professor == null)
             {
                 await servicoClassroom.Courses.Teachers.Create(new Teacher { UserId = ownerId }, cursoId.ToString()).ExecuteAsync();
             }
-            var requestUpdate = servicoClassroom.Courses.Patch(new Course() { OwnerId = ownerId, }, cursoId.ToString());
-            requestUpdate.UpdateMask = "ownerId";
-            await requestUpdate.ExecuteAsync();
+            var course = servicoClassroom.Courses.Get(cursoId.ToString()).Execute();
+            if (course != null && course.OwnerId != ownerId)
+            {
+                var requestUpdate = servicoClassroom.Courses.Patch(new Course() { OwnerId = ownerId, }, cursoId.ToString());
+                requestUpdate.UpdateMask = "ownerId";
+                await requestUpdate.ExecuteAsync();
+            }
         }
     }
 }
