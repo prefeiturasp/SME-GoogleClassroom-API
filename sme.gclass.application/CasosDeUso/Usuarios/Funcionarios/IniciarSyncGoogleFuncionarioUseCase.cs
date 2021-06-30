9a616f3d59bd96776d09190070d3d83f2237401b
Robson <robson.silva@amcom.com.br>
2021-06-30 15:29:39 -0300

    Bug: 42737 Ajuste na inserção da funcionalidade de inserção de professores, tratando usuário existente e definindo o id do google classroom. Incluimos a possibilidade de efetuar a sincronização apenas de um professor à partir do RF, com a criação do endpoint que permite executar dessa forma. Incluimos a funcionalidade de verificação da existência do professor e curso no google.

--- sme.gclass.application/CasosDeUso/Usuarios/Funcionarios/IniciarSyncGoogleFuncionarioUseCase.cs
+++ sme.gclass.application/CasosDeUso/Usuarios/Funcionarios/IniciarSyncGoogleFuncionarioUseCase.cs
@@ -1,7 +1,9 @@
 ﻿using MediatR;
+using Newtonsoft.Json;
 using SME.GoogleClassroom.Aplicacao.Interfaces;
 using SME.GoogleClassroom.Dominio;
 using SME.GoogleClassroom.Infra;
+using System;
 using System.Threading.Tasks;
 
 namespace SME.GoogleClassroom.Aplicacao
@@ -18,12 +20,29 @@ namespace SME.GoogleClassroom.Aplicacao
         public async Task<bool> Executar()
         {
             var publicarSyncFuncionario = await mediator.Send(new PublicaFilaRabbitCommand(RotasRabbit.FilaFuncionarioSync, RotasRabbit.FilaFuncionarioSync, true));
+
             if (!publicarSyncFuncionario)
-            {
                 throw new NegocioException("Não foi possível iniciar a sincronização de funcionários.");
-            }
 
             return publicarSyncFuncionario;
+        }
+
+        public async Task<bool> Executar(long rf)
+        {
+            ValidarRF(rf);
+
+            var filaProfessorSync = await mediator.Send(new PublicaFilaRabbitCommand(RotasRabbit.FilaProfessorSync, RotasRabbit.FilaProfessorSync, rf));
+
+            if (!filaProfessorSync)
+                throw new NegocioException($"Não foi possível iniciar a sincronização do funcionário RF {rf}.");
+
+            return filaProfessorSync;            
+        }
+
+        public void ValidarRF(long rf)
+        {
+            if (rf < 1)
+                throw new ArgumentNullException(nameof(rf), "O RF deve ser informado.");
         }
     }
 }
