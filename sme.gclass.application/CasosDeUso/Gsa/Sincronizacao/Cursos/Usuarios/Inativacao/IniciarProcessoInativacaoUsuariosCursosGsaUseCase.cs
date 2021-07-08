1814f269e230ae7cce01a1fc173fc0ae7a6c1791
Rodrigo Galante <rodrigo.galante@globo.com>
2021-07-08 15:14:34 -0300

    Implementação de serviço para iniciar processo de Inativação de usuários (Alunos)

---
+++ sme.gclass.application/CasosDeUso/Gsa/Sincronizacao/Cursos/Usuarios/Inativacao/IniciarProcessoInativacaoUsuariosCursosGsaUseCase.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Sentry;
+using SME.GoogleClassroom.Infra;
+using System;
+using System.Threading.Tasks;
+
+namespace SME.GoogleClassroom.Aplicacao
+{
+    public class IniciarProcessoInativacaoUsuariosCursosGsaUseCase : IIniciarProcessoInativacaoUsuariosCursosGsaUseCase
+    {
+        private readonly IMediator mediator;
+
+        public IniciarProcessoInativacaoUsuariosCursosGsaUseCase(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        public async Task<bool> Executar()
+        {
+            try
+            {
+                var dto = new FiltroInativacaoUsuariosCursosGoogleDto();
+                return await mediator.Send(new PublicaFilaRabbitCommand(RotasRabbit.FilaGsaInativacaoUsuarioCurso, RotasRabbit.FilaGsaInativacaoUsuarioCurso, dto));
+            }
+            catch (Exception ex)
+            {
+                SentrySdk.CaptureException(ex);
+                return false;
+            }
+        }
+    }
+}
