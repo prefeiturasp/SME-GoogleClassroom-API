ffa98349c1d0ee994f83ce3ea7b3ba58c53b6727
krisamcom <68442245+krisamcom@users.noreply.github.com>
2021-07-08 19:11:41 -0300

    bug: 42454 - ajustes

--- sme.gclass.application/CasosDeUso/Cursos/Alunos/AtualizarAlunosCursosUseCase.cs
+++ sme.gclass.application/CasosDeUso/Cursos/Alunos/AtualizarAlunosCursosUseCase.cs
@@ -1,27 +1,23 @@
-﻿using Google;
-using MediatR;
-using SME.GoogleClassroom.Aplicacao.Interfaces;
-using SME.GoogleClassroom.Dominio;
+﻿using SME.GoogleClassroom.Aplicacao.Interfaces;
 using SME.GoogleClassroom.Infra;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace SME.GoogleClassroom.Aplicacao
 {
     public class AtualizarAlunosCursosUseCase : IAtualizarAlunosCursosUseCase
-    {
-        private readonly IMediator mediator;
+    {        
         private readonly IObterCursosAlunosParaIncluirGoogleUseCase obterCursosAlunosParaIncluirGoogleUseCase;
         private readonly IObterCursosCadastradosUseCase obterCursosCadastradosUseCase;
+        private readonly IEnviarRequisicaoAtribuirAlunoCursoUseCase enviarRequisicaoAtribuirAlunoCursoUseCase;
 
-        public AtualizarAlunosCursosUseCase(IMediator mediator, IObterCursosAlunosParaIncluirGoogleUseCase obterCursosAlunosParaIncluirGoogleUseCase, IObterCursosCadastradosUseCase obterCursosCadastradosUseCase)
-        {
-            this.mediator = mediator;
+        public AtualizarAlunosCursosUseCase(IObterCursosAlunosParaIncluirGoogleUseCase obterCursosAlunosParaIncluirGoogleUseCase, 
+                                            IObterCursosCadastradosUseCase obterCursosCadastradosUseCase,
+                                            IEnviarRequisicaoAtribuirAlunoCursoUseCase enviarRequisicaoAtribuirAlunoCursoUseCase)
+        {           
             this.obterCursosAlunosParaIncluirGoogleUseCase = obterCursosAlunosParaIncluirGoogleUseCase;
             this.obterCursosCadastradosUseCase = obterCursosCadastradosUseCase;
+            this.enviarRequisicaoAtribuirAlunoCursoUseCase = enviarRequisicaoAtribuirAlunoCursoUseCase;
         }
 
         public async Task<bool> Executar(long? turmaId, long? componenteCurricularId)
@@ -43,14 +39,14 @@ namespace SME.GoogleClassroom.Aplicacao
                 {
                     var alunosCurso = await obterCursosAlunosParaIncluirGoogleUseCase.Executar(cursoGoogle.TurmaId, cursoGoogle.ComponenteCurricularId);
                     foreach (var aluno in alunosCurso)
-                    {
-                        var alunoConsulta = await mediator.Send(new ObterAlunosPorCodigosQuery(aluno.CodigoAluno));
-                        if (aluno is null || !alunoConsulta.Any()) continue;
-
-                        var existeProfessorCurso = await mediator.Send(new ExisteAlunoCursoGoogleQuery(alunoConsulta.First().Indice, cursoGoogle.Id));
-                        if (existeProfessorCurso) continue;
-
-                        await InserirAlunoCursoGoogleAsync(alunoConsulta.First(), cursoGoogle);
+                    {                        
+                        var alunoDto = new AtribuirAlunoCursoDto
+                        {
+                            CodigoAluno = aluno.CodigoAluno,
+                            TurmaId = aluno.TurmaId,
+                            ComponenteCurricularId = aluno.ComponenteCurricularId
+                        };
+                        await enviarRequisicaoAtribuirAlunoCursoUseCase.Executar(alunoDto);                        
                     }
                 }
                 catch (Exception ex)
@@ -60,31 +56,6 @@ namespace SME.GoogleClassroom.Aplicacao
             }
 
             return true;
-        }
-        private async Task InserirAlunoCursoGoogleAsync(AlunoGoogle alunoGoogle, CursoGoogle cursoGoogle)
-        {
-            var alunoCursoGoogle = new AlunoCursoGoogle(alunoGoogle.Indice, cursoGoogle.Id);
-
-            try
-            {
-                var professorCursoSincronizado = await mediator.Send(new InserirAlunoCursoGoogleCommand(alunoCursoGoogle, alunoGoogle.Email));
-                if (professorCursoSincronizado)
-                {
-                    await InserirAlunoCursoAsync(alunoCursoGoogle);
-                }
-            }
-            catch (GoogleApiException gEx)
-            {
-                if (gEx.EhErroDeDuplicidade())
-                    await InserirAlunoCursoAsync(alunoCursoGoogle);
-                else
-                    throw;
-            }
-        }
-
-        private async Task InserirAlunoCursoAsync(AlunoCursoGoogle alunoCursoGoogle)
-        {
-            alunoCursoGoogle.Id = await mediator.Send(new IncluirCursoUsuarioCommand(alunoCursoGoogle.UsuarioId, alunoCursoGoogle.CursoId));
         }
     }
 }
