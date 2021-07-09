071f259c277937af4ac81c5ab60467fa99f43f2c
elitonds <elitonds@gmail.com>
2021-07-09 17:02:24 -0300

    modelagem

--- sme.gclass.api.dados/Repositorios/GoogleClassroom/Usuarios/RepositorioAlunoInativoErro.cs
+++ sme.gclass.api.dados/Repositorios/GoogleClassroom/Usuarios/RepositorioAlunoInativoErro.cs
@@ -1,5 +1,10 @@
-﻿using SME.GoogleClassroom.Dados.Interfaces;
+﻿using Dapper;
+using SME.GoogleClassroom.Dados.Interfaces;
+using SME.GoogleClassroom.Dominio;
 using SME.GoogleClassroom.Infra;
+using System;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace SME.GoogleClassroom.Dados
 {
@@ -8,6 +13,53 @@ namespace SME.GoogleClassroom.Dados
         public RepositorioAlunoInativoErro(ConnectionStrings connectionStrings)
             : base(connectionStrings)
         {
+        }
+
+        public async Task<PaginacaoResultadoDto<CursoUsuarioRemovidoConsultaDto>> ObterAlunosCursosRemovidosPorCursoId(Paginacao paginacao, string cursoId)
+        {
+            var query = new StringBuilder(@"SELECT
+                                                   ui.usuario_id UsuarioId,
+                                                   ui.removido_em RemovidoEm,
+                                                   u.google_classroom_id UsuarioGsaId,
+                                                   u.email EmailUsuario,
+                                                   u.nome NomeUsuario
+                                              FROM usuario_inativado_gsa ui
+                                              inner join usuarios u on u.indice = ui.usuario_id 
+                                             WHERE ui.usuario_tipo = @tipo ");
+
+            var queryCount = new StringBuilder("SELECT count(*) from usuario_inativado_gsa u WHERE u.usuario_tipo = @tipo ");
+
+            if (!string.IsNullOrEmpty(cursoId))
+            {
+                query.AppendLine("AND u.curso_id = @cursoId");
+                queryCount.AppendLine("AND u.curso_id = @cursoId");
+            }
+
+            if (paginacao.QuantidadeRegistros > 0)
+                query.AppendLine($" OFFSET @quantidadeRegistrosIgnorados ROWS FETCH NEXT @quantidadeRegistros ROWS ONLY ");
+
+            query.AppendLine(";");
+            query.AppendLine(queryCount.ToString());
+
+            var retorno = new PaginacaoResultadoDto<CursoUsuarioRemovidoConsultaDto>();
+
+            var parametros = new
+            {
+                quantidadeRegistrosIgnorados = paginacao.QuantidadeRegistrosIgnorados,
+                quantidadeRegistros = paginacao.QuantidadeRegistros,
+                tipo = UsuarioTipo.Aluno,
+                cursoId
+            };
+
+            using var conn = ObterConexao();
+
+            using var registros = await conn.QueryMultipleAsync(query.ToString(), parametros);
+
+            retorno.Items = registros.Read<CursoUsuarioRemovidoConsultaDto>();
+            retorno.TotalRegistros = registros.ReadFirst<int>();
+            retorno.TotalPaginas = (int)Math.Ceiling((double)retorno.TotalRegistros / paginacao.QuantidadeRegistros);
+
+            return retorno;
         }
 
     }
---
+++ sme.gclass.api.domain/Entidades/Gsa/Usuarios/Inativados/UsuarioInativado.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.GoogleClassroom.Dominio
+{
+    public class UsuarioInativado
+    {
+        public long Id { get; set; }
+        public long UsuarioId { get; set; }
+        public UsuarioTipo usuario_tipo { get; set; }
+        public DateTime RemovidoEm { get; set; }
+    }
+}
