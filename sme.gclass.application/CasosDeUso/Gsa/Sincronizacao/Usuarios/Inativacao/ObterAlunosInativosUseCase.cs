e85e71ff665bc091afdb44d695e7bdb1ad8f1ba0
Rodrigo Galante <rodrigo.galante@globo.com>
2021-07-15 01:07:00 -0300

    Endpoint para listar os alunos inativados

---
+++ sme.gclass.application/CasosDeUso/Gsa/Sincronizacao/Usuarios/Inativacao/ObterAlunosInativosUseCase.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using SME.GoogleClassroom.Aplicacao.Queries;
+using SME.GoogleClassroom.Dominio;
+using SME.GoogleClassroom.Infra;
+using System;
+using System.Threading.Tasks;
+
+namespace SME.GoogleClassroom.Aplicacao
+{
+    public class ObterAlunosInativosUseCase : IObterAlunosInativosUseCase
+    {
+        private readonly IMediator mediator;
+
+        public ObterAlunosInativosUseCase(IMediator mediator)
+        {
+            this.mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        }
+
+        public async Task<PaginacaoResultadoDto<UsuarioInativo>> Executar(FiltroObterAlunosInativosDto filtro)
+        {
+            var paginacao = new Paginacao(filtro.PaginaNumero, filtro.RegistrosQuantidade);
+            return await mediator.Send(new ObterAlunosInativosQuery(paginacao));
+        }
+    }
+
+    
+}
