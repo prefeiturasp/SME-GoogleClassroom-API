dbf94c04a0f07badb542e46496c79c88ddf85ab6
BernardRibeiro <bernard.ribeiro@gmail.com>
2021-07-16 15:26:58 -0300

    Update scripts/V42__CONTROLE_REMOCAO_PROFESSORES.sql
    Co-authored-by: Rafael Losi <57261028+rafalosiamcom@users.noreply.github.com>

--- scripts/V42__CONTROLE_REMOCAO_PROFESSORES.sql
+++ scripts/V42__CONTROLE_REMOCAO_PROFESSORES.sql
@@ -1,4 +1,4 @@
 insert into execucao_controle 
 (execucao_tipo, ultima_execucao)
-	select 14, '2021-01-01' 
-	  where not exists(select 1 from execucao_controle where execucao_tipo = 14);
+	select 15, '2021-01-01' 
+	  where not exists(select 1 from execucao_controle where execucao_tipo = 15);
