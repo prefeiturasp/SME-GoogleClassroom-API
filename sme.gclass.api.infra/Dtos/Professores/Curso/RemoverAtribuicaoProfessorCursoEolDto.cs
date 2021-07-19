b5fccd555764762b098cd8a62037556c3dea5619
Rafael Losi <rafael.losi@amcom.com.br>
2021-07-19 10:44:22 -0300

    36779 DTO de Professores a remover do curso

---
+++ sme.gclass.api.infra/Dtos/Professores/Curso/RemoverAtribuicaoProfessorCursoEolDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SME.GoogleClassroom.Infra
+{
+    public class RemoverAtribuicaoProfessorCursoEolDto
+    {
+        public long TurmaCodigo { get; set; }
+        public long ComponenteCurricularCodigo { get; set; }
+        public string UsuarioRf { get; set; }
+        public DateTime DataDisponibilizacao { get; set; }
+        public int MotivoDisponibilizacao { get; set; }
+    }
+}
