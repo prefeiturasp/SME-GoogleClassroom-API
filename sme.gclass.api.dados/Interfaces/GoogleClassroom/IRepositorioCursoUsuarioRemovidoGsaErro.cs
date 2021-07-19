c53eeabc049bfe81f38d1eb857eb8ea24e7540e5
Diego Delmiro <diego.delmiro@amcom.com.br>
2021-07-19 17:47:47 -0300

    43849 criando tratativa de erro para remoçao de aluno curso erro

--- sme.gclass.api.dados/Interfaces/GoogleClassroom/IRepositorioCursoUsuarioRemovidoGsaErro.cs
+++ sme.gclass.api.dados/Interfaces/GoogleClassroom/IRepositorioCursoUsuarioRemovidoGsaErro.cs
@@ -1,4 +1,5 @@
-﻿using SME.GoogleClassroom.Dominio;
+﻿using System.Collections.Generic;
+using SME.GoogleClassroom.Dominio;
 using System.Threading.Tasks;
 
 namespace SME.GoogleClassroom.Dados
@@ -6,5 +7,7 @@ namespace SME.GoogleClassroom.Dados
     public interface IRepositorioCursoUsuarioRemovidoGsaErro
     {
         Task<long> SalvarAsync(CursoUsuarioRemovidoGsaErro usuarioCursoGsa);
+        Task<IEnumerable<CursoUsuarioRemovidoGsaErro>> ObterTodos();
+        Task<int> Excluir(long usuarioId, long cursoId);
     }
 }
--- sme.gclass.api.worker.rabbit/Controllers/AlunoController.cs
+++ sme.gclass.api.worker.rabbit/Controllers/AlunoController.cs
@@ -180,5 +180,21 @@ namespace SME.GoogleClassroom.Worker.Rabbit.Controllers
             var retorno = await useCase.Executar(filtro);
             return Ok(retorno);
         }
+        
+        /// <summary>
+        /// Inicia o tratamento de erros de alunos removidos do EOL para o Google Classroom.
+        /// </summary>
+        /// <remarks>
+        /// **Importante:** Visando a melhoria de performance, o tratamento de erros acontece de forma assíncrona e descentralizada,
+        /// não sendo possível assim acompanhar em tempo real sua evolução.
+        /// </remarks>
+        /// <response code="200">O início da sincronização ocorreu com sucesso.</response>
+        [HttpPost("removidos/erros/tratamentos")]
+        [ProducesResponseType(typeof(bool), 200)]
+        public async Task<IActionResult> ProcessarErrosDeRemocaoAluno([FromServices] IIniciarSyncGoogleAlunosRemovidosErrosUseCase iniciarSyncGoogleAlunosRemovidosErrosUseCase)
+        {
+            var retorno = await iniciarSyncGoogleAlunosRemovidosErrosUseCase.Executar();
+            return Ok(retorno);
+        }
     }
 }
