f8ec2f4bfb4610c80072b7a9ae9f6d48580d41c5
Rafael Losi <rafael.losi@amcom.com.br>
2021-07-20 18:02:43 -0300

    43814 Criado endpoint de listagem de professores a remover atribuiçao

--- sme.gclass.api.ioc/RegistraDependencias.cs
+++ sme.gclass.api.ioc/RegistraDependencias.cs
@@ -133,6 +133,9 @@ namespace SME.GoogleClassroom.IoC
             services.TryAddScoped<IAtualizarAlunosCursosUseCase, AtualizarAlunosCursosUseCase>();
             services.TryAddScoped<IObterAlunosQueSeraoRemovidosUseCase, ObterAlunosQueSeraoRemovidosUseCase>();
 
+            // Remover atribuicao professor x curso
+            services.TryAddScoped<IObterProfessoresQueSeraoRemovidosUseCase, ObterProfessoresQueSeraoRemovidosUseCase>();
+
             RegistrarCasosDeUsoGsa(services);
         }
 
