7a966f72e375286348b54715061576658feb1643
Diego Delmiro <diego.delmiro@amcom.com.br>
2021-07-30 10:26:27 -0300

    45001 ajustando valor de dias do parametro DiasRemocaoFuncionario

--- scripts/V47__INSERINDO_PARAMETRO_DIA_REMOCAO_FUNCIONARIO.sql
+++ scripts/V47__INSERINDO_PARAMETRO_DIA_REMOCAO_FUNCIONARIO.sql
@@ -1,3 +1,3 @@
 ﻿delete from public.parametro_sistema where tipo = 2;
 insert into public.parametro_sistema (nome, tipo, descricao, valor, ano, ativo)
-values('DiasRemocaoFuncionario', 2, 'Parametro de dias para remoção de funcionario', '1', 2021, true);
+values('DiasRemocaoFuncionario', 2, 'Parametro de dias para remoção de funcionario', '15', 2021, true);
