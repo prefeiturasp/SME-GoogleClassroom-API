7825b4c5c97c70a4fb42090b9cea2f7549fefbde
Rafael Losi <rafael.losi@amcom.com.br>
2021-08-03 10:21:09 -0300

    36780 Corrigido carga da data

--- sme.gclass.application/CasosDeUso/Gsa/Sincronizacao/Cursos/Usuarios/Remocao/TratarFuncionarioRemovidosCursosUseCase.cs
+++ sme.gclass.application/CasosDeUso/Gsa/Sincronizacao/Cursos/Usuarios/Remocao/TratarFuncionarioRemovidosCursosUseCase.cs
@@ -27,7 +27,7 @@ namespace SME.GoogleClassroom.Aplicacao
             try
             {
                 var funcionariosASeremRemovidos = await mediator.Send(
-                    new ObterFuncionariosParaRemoverCursoQuery(dto.TurmaId.ToString(), dto.DatasAluno.DataInicio, dto.DatasAluno.DataFim));
+                    new ObterFuncionariosParaRemoverCursoQuery(dto.TurmaId.ToString(), dto.DatasFuncionario.DataInicio, dto.DatasFuncionario.DataFim));
 
                 foreach (var funcionarioASeremRemovido in funcionariosASeremRemovidos)
                 {
