7e205d62403d2090837e9d15d7416963481ba96c
Diego Delmiro <diego.delmiro@amcom.com.br>
2021-08-04 14:19:06 -0300

    45027 ajuste no parametro

--- scripts/V53__INSERINDO_PARAMETROS_CARGA_INICIAL.sql
+++ scripts/V53__INSERINDO_PARAMETROS_CARGA_INICIAL.sql
@@ -1,3 +1,3 @@
 ﻿insert
 into carga_inicial (ano, tipos_ue, ues, turmas, criado_em)
-values (2022, '1,2,3,4,10,11,12,13,16,17,18,23,28,31', null, null, current_date);
+values (2021, '1,2,3,4,10,11,12,13,16,17,18,23,28,31', null, null, current_date);
