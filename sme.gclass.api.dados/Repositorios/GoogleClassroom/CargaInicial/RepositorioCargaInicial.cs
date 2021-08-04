963080ed3edeb9fbd985df49d26398b204e77ae8
Rafael Losi <rafael.losi@amcom.com.br>
2021-08-04 09:14:07 -0300

    45025 Modelagem Carga Inicial

---
+++ sme.gclass.api.dados/Repositorios/GoogleClassroom/CargaInicial/RepositorioCargaInicial.cs
@@ -0,0 +1,12 @@
+﻿using SME.GoogleClassroom.Dados.Interfaces;
+using SME.GoogleClassroom.Infra;
+
+namespace SME.GoogleClassroom.Dados
+{
+    public class RepositorioCargaInicial : RepositorioGoogle, IRepositorioCargaInicial
+    {
+        public RepositorioCargaInicial(ConnectionStrings connectionStrings) : base(connectionStrings)
+        {
+        }
+    }
+}
