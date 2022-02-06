115e06938fccffe40d267c942281d1a3527a8f24
Rafael Losi <rafael.losi@amcom.com.br>
2022-02-06 03:34:24 -0300

    AB#58376 Melhora performance querys Cursos

--- sme.gclass.application/CasosDeUso/Usuarios/Alunos/IncluirAlunoUseCase.cs
+++ sme.gclass.application/CasosDeUso/Usuarios/Alunos/IncluirAlunoUseCase.cs
@@ -1,7 +1,5 @@
 ﻿using Google;
 using MediatR;
-using Newtonsoft.Json;
-using SME.GoogleClassroom.Aplicacao.Commands.Usuarios.ConsultarUsuarioGoogleClassroom;
 using SME.GoogleClassroom.Dominio;
 using SME.GoogleClassroom.Infra;
 using System;
@@ -13,12 +11,10 @@ namespace SME.GoogleClassroom.Aplicacao
     public class IncluirAlunoUseCase : IIncluirAlunoUseCase
     {
         private readonly IMediator mediator;
-        private readonly bool _deveExecutarIntegracao;
 
         public IncluirAlunoUseCase(IMediator mediator, VariaveisGlobaisOptions variaveisGlobaisOptions)
         {
             this.mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
-            _deveExecutarIntegracao = variaveisGlobaisOptions.DeveExecutarIntegracao;
         }
 
         public async Task<bool> Executar(MensagemRabbit mensagemRabbit)
