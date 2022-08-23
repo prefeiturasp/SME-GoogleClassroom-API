b101eac375152eecbae096f43557e0a99a210c29
Vinícius Adelir Nyari <47329467+viniciusnyari@users.noreply.github.com>
2022-08-23 10:33:03 -0300

    Evitar que script de criação dos parâmetros do sistema não execute mais de uma vez

--- scripts/V66__CRIAR_PARAMETROS_CONFIG_CELP.sql
+++ scripts/V66__CRIAR_PARAMETROS_CONFIG_CELP.sql
@@ -1,2 +1,15 @@
-insert into parametro_sistema (nome, tipo,descricao, valor,ano,ativo) values ('EmailCoordenadorCELP',7,'Parametro usado para indicar o e-mail do coordenador CELP','ana.katy@sme.prefeitura.sp.gov.br',2022,true);
-insert into parametro_sistema (nome, tipo,descricao, valor,ano,ativo) values ('ComponentesCurricularesCELP',8,'Parametro usado para indicar os componentes curriculares que serão importados pelo CELP','1657,1658,1659,1660,1661,1662',2022,true);
+INSERT INTO public.parametro_sistema
+    (nome, tipo,descricao, valor,ano,ativo)
+SELECT 'EmailCoordenadorCELP',7,'Parametro usado para indicar o e-mail do coordenador CELP','ana.katy@sme.prefeitura.sp.gov.br',2022,true
+WHERE
+    NOT EXISTS (
+        SELECT ano FROM public.parametro_sistema WHERE ano = 2022 and tipo = 7
+    );
+	
+INSERT INTO public.parametro_sistema
+    (nome, tipo,descricao, valor,ano,ativo)
+SELECT 'ComponentesCurricularesCELP',8,'Parametro usado para indicar os componentes curriculares que serão importados pelo CELP','1657,1658,1659,1660,1661,1662',2022,true
+WHERE
+    NOT EXISTS (
+        SELECT ano FROM public.parametro_sistema WHERE ano = 2022 and tipo = 8
+    );
