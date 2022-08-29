770db859081df0914285a082605129f37a6fac18
Vinícius Adelir Nyari <47329467+viniciusnyari@users.noreply.github.com>
2022-08-29 14:42:48 -0300

    Corrigindo o e-mail do coordenador no parâmetro

--- scripts/V66__CRIAR_PARAMETROS_CONFIG_CELP.sql
+++ scripts/V66__CRIAR_PARAMETROS_CONFIG_CELP.sql
@@ -1,6 +1,6 @@
 INSERT INTO public.parametro_sistema
     (nome, tipo,descricao, valor,ano,ativo)
-SELECT 'EmailCoordenadorCELP',7,'Parametro usado para indicar o e-mail do coordenador CELP','ana.katy@sme.prefeitura.sp.gov.br',2022,true
+SELECT 'EmailCoordenadorCELP',7,'Parametro usado para indicar o e-mail do coordenador CELP','anagabriel.8078424@edu.sme.prefeitura.sp.gov.br',2022,true
 WHERE
     NOT EXISTS (
         SELECT ano FROM public.parametro_sistema WHERE ano = 2022 and tipo = 7
