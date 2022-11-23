6b6e12658f972fb7e795cb48bbd64d6b0973fc48
DanylloAntunes <danyllo.antunes@gmail.com>
2022-11-23 17:49:22 -0300

    79604 - Criar endpoint para healthcheck do GCA

---
+++ sme.gclass.api.ioc/Extensions/NpgSqlHealthCheckExtension.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace SME.GoogleClassroom.IoC
+{
+    public static class NpgSqlHealthCheckExtension
+    {
+        public static IHealthChecksBuilder AddPostgresGoogleClassroom(this IHealthChecksBuilder builder, IConfiguration configuration)
+        {
+            return AddPostgres(builder, configuration.GetConnectionString("GoogleClassroomConnection"), "Postgres_GoogleClassroom");
+        }
+
+        public static IHealthChecksBuilder AddPostgresApiEol(this IHealthChecksBuilder builder, IConfiguration configuration)
+        {
+            return AddPostgres(builder, configuration.GetConnectionString("ApiEolConnection"), "Postgres_ApiEol");
+        }
+
+        private static IHealthChecksBuilder AddPostgres(IHealthChecksBuilder builder, string connectionString, string nome)
+        {
+            return builder.AddNpgSql(connectionString,
+                                     name: nome,
+                                     failureStatus: HealthStatus.Unhealthy);
+        }
+    }
+}
