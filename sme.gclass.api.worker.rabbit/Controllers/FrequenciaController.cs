72ae05488df6e28ab02e9ba582da467a8f1fb19e
Vinícius Adelir Nyari <47329467+viniciusnyari@users.noreply.github.com>
2023-01-18 13:54:35 -0300

    Criação do endpoint de persistência das frequências

---
+++ sme.gclass.api.worker.rabbit/Controllers/FrequenciaController.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SME.GoogleClassroom.Dominio;
+using SME.GoogleClassroom.Infra;
+using SME.GoogleClassroom.Worker.Rabbit.Filters;
+
+namespace SME.GoogleClassroom.Worker.Rabbit.Controllers
+{
+    /// <summary>
+    /// Frequencias
+    /// </summary>
+    [ApiController]
+    [ChaveIntegracaoGoogleClassroomApi]
+    [Route("api/v1/frequencias")]
+    public class FrequenciaController : Controller
+    {
+        /// <summary>
+        /// Realiza o lançamento da frequência.
+        /// </summary>
+        /// <response code="200">A consulta foi realizada com sucesso.</response>
+        /// <response code="500">Ocorreu um erro inesperado durante a consulta.</response>
+        /// <response code="601">Houve uma falha de validação durante a consulta.</response>
+        [HttpPost]
+        [ProducesResponseType(typeof(PaginacaoResultadoDto<AlunoEol>), 200)]
+        [ProducesResponseType(typeof(RetornoBaseDto), 500)]
+        [ProducesResponseType(typeof(RetornoBaseDto), 601)]
+        public async Task<IActionResult> LancamentoFrequencia([FromServices] ILancarFrequenciaUseCase lancarFrequenciaUseCase,
+            [FromQuery] IEnumerable<FrequenciaSalvarAulaAlunosDto> )
+        {
+            return Ok(await lancarFrequenciaUseCase.Executar(filtro));
+        }
+    }
+}
