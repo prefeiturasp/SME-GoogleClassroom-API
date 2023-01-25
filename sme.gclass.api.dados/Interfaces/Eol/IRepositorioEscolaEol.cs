a14062eeea4f42b5e2b3a285270437042eeb76d7
jailsvs <jailsvs@gmail.com>
2023-01-25 13:35:02 -0300

    84810 - End point busca de escolas

--- sme.gclass.api.dados/Interfaces/Eol/IRepositorioEscolaEol.cs
+++ sme.gclass.api.dados/Interfaces/Eol/IRepositorioEscolaEol.cs
@@ -1,9 +1,12 @@
-﻿using System.Threading.Tasks;
+﻿using SME.GoogleClassroom.Infra;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace SME.GoogleClassroom.Dados.Interfaces.Eol
 {
     public interface IRepositorioEscolaEol
     {
         public Task<int> ObterTipoDaEscolaPorCodigoEscola(string codigoEscola);
+        public Task<IEnumerable<EscolaDTO>> ObterEscolas();
     }
 }
