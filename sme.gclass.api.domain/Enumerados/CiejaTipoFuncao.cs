3c6dee88f8f5e7b9cc0554521cf5975d7fa33cb7
Thiago Fernando <thiago.fernando@msn.com>
2023-01-27 08:07:13 -0300

    Ajustes para Obter Função do Funcionário na UE Cieja

---
+++ sme.gclass.api.domain/Enumerados/CiejaTipoFuncao.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SME.GoogleClassroom.Dominio.Enumerados
+{
+    public enum CiejaTipoFuncao
+    {
+        [Display(Name = "CIEJA COORDENADOR GERAL", ShortName = "DIRETOR")]
+        Diretor = 42,
+        [Display(Name = "CIEJA ASSIST COORD GERAL", ShortName = "AD")]
+        Ad = 43,
+        [Display(Name = "CIEJA ASSIST PED E EDUCACIONAL", ShortName = "CP")]
+        Cp = 44,
+    }
+}
