ea0d05241435adec762871a20dabe73dd7a0dabb
Vinícius Adelir Nyari <47329467+viniciusnyari@users.noreply.github.com>
2023-02-13 22:14:33 -0300

    Correção do agrupamento de componentes do território do saber

---
+++ sme.gclass.api.infra/Dtos/ComponenteCurricular/ComponenteTerritorioTurmaDto.cs
@@ -0,0 +1,17 @@
+﻿using Nest;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.GoogleClassroom.Infra
+{
+    public class ComponenteTerritorioTurmaDto
+    {
+        public string NomeComponenteCurricular { get; set; }
+        public long ComponenteCurricularCodigo { get; set; }
+        public long ComponenteCurricularCodigoUnico { get; set; }
+        public string RegistroFuncional { get; set; }
+        public DateTime? DataDisponibizacao { get; set; }
+        public long ComponenteCurricularCodigoEol { get; set; }
+    }
+}
