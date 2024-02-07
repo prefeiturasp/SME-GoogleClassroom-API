069c425cd6d55d0158ba3c2fa35571a03702027d
Thiago Fernando <thiago.fernando@msn.com>
2024-02-07 10:17:42 -0300

    verifica se o CPF possui 11 dígitos, calcula os dígitos verificadores e confere se eles correspondem aos dígitos finais do CPF

--- sme.gclass.application/CasosDeUso/Gsa/Sincronizacao/Usuarios/AtualizarEmailUsuarioUseCase.cs
+++ sme.gclass.application/CasosDeUso/Gsa/Sincronizacao/Usuarios/AtualizarEmailUsuarioUseCase.cs
@@ -31,9 +31,20 @@ namespace SME.GoogleClassroom.Aplicacao
                 await mediator.Send(new AtualizarInserirEmailUsuarioCommand(usuario));
         }
 
-        private static void ValidarSePossuirErros(InserirAtualizarEmailDTO filtro)
+        private  void ValidarSePossuirErros(InserirAtualizarEmailDTO filtro)
         {
             var erros = new List<string>();
+
+
+            var listaDeCpfs = filtro.Usuarios.Select(x => new { x.Cpf, x.Nome });
+
+            foreach (var e in listaDeCpfs)
+            {
+                var valido = ValidaCPF(e.Cpf);
+                if(!valido)
+                    erros.Add($"Informe um CPF válido para o usuário {e.Nome} ");
+            }
+
 
             var tiposQueDevemTerId = new List<UsuarioTipo>() { UsuarioTipo.Aluno,UsuarioTipo.Professor, UsuarioTipo.Funcionario };
             var tiposQueDevemTerCpf = new List<UsuarioTipo>() { UsuarioTipo.FuncionarioIndireto };
@@ -61,5 +72,48 @@ namespace SME.GoogleClassroom.Aplicacao
             if (erros.PossuiElementos())
                 throw new NegocioException(erros);
         }
+        public bool ValidaCPF(string cpf)
+        {
+            int[] multiplicador1 = new int[9] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] multiplicador2 = new int[10] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+
+            cpf = cpf.Trim();
+            cpf = cpf.Replace(".", "").Replace("-", "");
+
+            if (cpf.Length != 11)
+                return false;
+
+            string tempCpf = cpf.Substring(0, 9);
+            int soma = 0;
+
+            for (int i = 0; i < 9; i++)
+                soma += int.Parse(tempCpf[i].ToString()) * multiplicador1[i];
+
+            int resto = soma % 11;
+
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+
+            string digito = resto.ToString();
+            tempCpf = tempCpf + digito;
+            soma = 0;
+
+            for (int i = 0; i < 10; i++)
+                soma += int.Parse(tempCpf[i].ToString()) * multiplicador2[i];
+
+            resto = soma % 11;
+
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+
+            digito = digito + resto.ToString();
+
+            return cpf.EndsWith(digito);
+        }
+
     }
 }
